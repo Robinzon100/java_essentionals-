d3e83146e4373f11d7158d4e4fc30eed6a7284af
LAB316 Student01 <lab316_student01@gau.local>
2019-09-23 18:29:56 +0400

    :tada: created a user object from a input

--- week_1/src/com/company/Main.java
+++ week_1/src/com/company/Main.java
@@ -6,23 +6,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-//        User newUser = new User("beqa", "arabidze",20);
-//        System.out.println(newUser.surname);
-//        newUser.isSwrul(17);
+        Scanner input = new Scanner(System.in);
 
-    //        int[] m1 = new int[4];
-    //        int[] m2 ={2,3,4,5};
-    //
-    //        for (int i = 0; i < m2.length; i++){
-    //            System.out.println(m2[i]);
-    //        }
-    //
-            //        Random random = new Random();
-            //        System.out.println(random.nextInt(100));
 
-        String s;
-        Scanner in = new Scanner(System.in);
-        s = in.nextLine();
-        System.out.println(s);
+        System.out.println("enter the subjects name : ");
+        String name = input.next();
+
+        System.out.println("enter the subjects surnmae : ");
+        String surname = input.next();
+
+        System.out.println("enter the subjects age : ");
+        String ageSting = input.next();
+        int age = Integer.parseInt(ageSting);
+
+        User newUser = new User(name, surname, age);
+        System.out.println(newUser.name + "is");
+        newUser.isSwrul(17);
+
+
+
     }
 }
