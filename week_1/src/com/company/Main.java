e183ca5ebda915d3fede8c0effd55c0eb8924f95
LAB316 Student01 <lab316_student01@gau.local>
2019-09-23 17:17:46 +0400

    :tada: initial commit

---
+++ week_1/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        User newUser = new User("beqa", "arabidze",20);
+        System.out.println(newUser.name);
+    }
+}
---
+++ week_1/src/com/company/User.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class User {
+    String name;
+    String surname;
+    String age;
+
+    public User(String name, String surname, int age) {
+        name = name;
+        surname = surname;
+        age = age;
+    }
+}
