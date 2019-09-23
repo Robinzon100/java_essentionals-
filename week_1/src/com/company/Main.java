3af761cc0a3471917335d60cf3b8c7e35696d78d
LAB316 Student01 <lab316_student01@gau.local>
2019-09-23 18:15:46 +0400

    :tada: created the input

--- week_1/src/com/company/Main.java
+++ week_1/src/com/company/Main.java
@@ -1,11 +1,28 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        User newUser = new User("beqa", "arabidze",20);
-        System.out.println(newUser.surname);
+//        User newUser = new User("beqa", "arabidze",20);
+//        System.out.println(newUser.surname);
+//        newUser.isSwrul(17);
 
-        newUser.isSwrul(17);
+    //        int[] m1 = new int[4];
+    //        int[] m2 ={2,3,4,5};
+    //
+    //        for (int i = 0; i < m2.length; i++){
+    //            System.out.println(m2[i]);
+    //        }
+    //
+            //        Random random = new Random();
+            //        System.out.println(random.nextInt(100));
+
+        String s;
+        Scanner in = new Scanner(System.in);
+        s = in.nextLine();
+        System.out.println(s);
     }
 }
